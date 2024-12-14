fd028f7e4ed9de455849f19086c8fce644242f09
Kudastech <kudastech@gmail.com>
2024-12-14 08:34:15 +0100

    The web route was modified

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UrlController::class, 'index']);
+Route::post('/shorten', [UrlController::class, 'store'])->name('url.shorten');
+Route::get('/{shortCode}', [UrlController::class, 'redirect'])->name('url.redirect');
